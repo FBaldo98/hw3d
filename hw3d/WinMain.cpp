d235e895faa31fbe09bf9ec3feeab1a04f3beb2c
FBaldo98 <francesco.baldisserri98@outlook.com>
2020-05-26 22:26:38 +0200

    Creates the Window instance

---
+++ hw3d/WinMain.cpp
@@ -0,0 +1,48 @@
+#include <Windows.h>
+
+int CALLBACK WinMain(
+	HINSTANCE	hInstance,
+	HINSTANCE	hPrevInstance,
+	LPSTR		plCmdLine,
+	int			nCmdShow)
+{
+
+	const auto pClassName = L"hw3dwnd";
+
+	// Create window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hIcon = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+
+	// Register window class
+	RegisterClassEx(&wc);
+
+	// Create the window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		L"Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr,
+		hInstance,
+		nullptr
+	);
+
+	// Show the window
+	ShowWindow(hWnd, SW_SHOW);
+
+	while (true);
+
+	return 0;
+}
