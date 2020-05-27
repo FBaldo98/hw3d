5242b9b9e81d3008a1155e0ec384f18b942041cc
FBaldo98 <francesco.baldisserri98@outlook.com>
2020-05-27 22:36:01 +0200

    Adds exception catching in the WinMai function

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -6,20 +6,35 @@ int CALLBACK WinMain(
 	_In_ LPSTR			plCmdLine,
 	_In_ int			nCmdShow)
 {
-
-	Window wnd(800, 300, L"Test Window");
-
-	MSG msg;
-	BOOL gResult;
-	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	if (gResult == -1)
+	try
 	{
-		return -1;
-	}
+		Window wnd(800, 300, L"Test Window");
 
-	return static_cast<int>(msg.wParam);
+		MSG msg;
+		BOOL gResult;
+		while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (gResult == -1)
+		{
+			return -1;
+		}
+
+		return static_cast<int>(msg.wParam);
+	}
+	catch (const BaldoException& e)
+	{
+		MessageBox(nullptr, reinterpret_cast<LPCWSTR>(e.what()), reinterpret_cast<LPCWSTR>(e.GetType()), MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch(const std::exception & e)
+	{
+		MessageBox(nullptr, reinterpret_cast<LPCWSTR>(e.what()), L"Standard exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (...)
+	{
+		MessageBox(nullptr, L"No details available", L"Standard exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	return -1;
 }
