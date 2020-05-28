7a91ab10ca9e4256f39b73d73f93708fec71743a
FBaldo98 <francesco.baldisserri98@outlook.com>
2020-05-28 20:41:25 +0200

    Mouse Leave/Enter and Mouse Drag
    Implements mouse leave and mouse enter events.
    Implements mouse capture if click and drag outside of client region.

--- hw3d/Mouse.cpp
+++ hw3d/Mouse.cpp
@@ -15,6 +15,11 @@ int Mouse::GetPosY() const noexcept
 	return y;
 }
 
+bool Mouse::IsInWindow() const noexcept
+{
+	return isInWindow;
+}
+
 bool Mouse::LeftIsPressed() const noexcept
 {
 	return leftIsPressed;
@@ -47,6 +52,21 @@ void Mouse::OnMouseMove(int x, int y) noexcept
 	this->y = y;
 
 	buffer.push(Mouse::Event(Mouse::Event::Type::Move, *this));
+	TrimBuffer();
+}
+
+void Mouse::OnMouseLeave() noexcept
+{
+	isInWindow = false;
+
+	buffer.push(Mouse::Event(Mouse::Event::Type::Leave, *this));
+	TrimBuffer();
+}
+
+void Mouse::OnMouseEnter() noexcept
+{
+	isInWindow = true;
+	buffer.push(Mouse::Event(Mouse::Event::Type::Enter, *this));
 	TrimBuffer();
 }
 
