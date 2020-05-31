bfc38c122dba3dd5c2afde5ccdd684dc4aad6f03
FBaldo98 <francesco.baldisserri98@outlook.com>
2020-05-31 22:14:03 +0200

    D3D Exception Handling

--- hw3d/Window.cpp
+++ hw3d/Window.cpp
@@ -108,6 +108,9 @@ std::optional<int> Window::ProcessMessages()
 
 Graphics& Window::Gfx()
 {
+	if (!pGfx) {
+		throw BHWND_NOGFX_EXCEPT();
+	}
 	return *pGfx;
 }
 
@@ -232,28 +235,6 @@ LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noe
 
 ////////// WINDOW EXCEPTION
 
-Window::Exception::Exception(int line, const char* file, HRESULT hr) noexcept 
-	: BaldoException(line, file),
-	hr(hr)
-{
-}
-
-const char* Window::Exception::what() const noexcept
-{
-	std::ostringstream oss;
-	oss << GetType() << std::endl
-		<< "[Error Code] " << GetErrorCode() << std::endl
-		<< "[Description] " << GetErrorString() << std::endl
-		<< GetOriginString();
-	whatBuffer = oss.str();
-	return whatBuffer.c_str();
-}
-
-const char* Window::Exception::GetType() const noexcept
-{
-	return "Baldo Window Exception";
-}
-
 std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 {
 	char* pMsgBuf = nullptr;
@@ -270,12 +251,40 @@ std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 	return errorString;
 }
 
-HRESULT Window::Exception::GetErrorCode() const noexcept
+Window::HrException::HrException(int line, const char* file, HRESULT hr) noexcept
+	: Exception(line, file),
+	hr(hr)
+{
+}
+
+const char* Window::HrException::what() const noexcept
+{
+	std::ostringstream oss;
+	oss << GetType() << std::endl
+		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
+		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
+		<< "[Description] " << GetErrorDescription() << std::endl
+		<< GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+}
+
+const char* Window::HrException::GetType() const noexcept
+{
+	return "Baldo Window Exception";
+}
+
+HRESULT Window::HrException::GetErrorCode() const noexcept
 {
 	return hr;
 }
 
-std::string Window::Exception::GetErrorString() const noexcept
+std::string Window::HrException::GetErrorDescription() const noexcept
 {
-	return TranslateErrorCode(hr);
+	return Exception::TranslateErrorCode(hr);
+}
+
+const char* Window::NoGfxException::GetType() const noexcept
+{
+	return "Baldo Window Exception [No Graphics]";
 }
--- hw3d/Window.h
+++ hw3d/Window.h
@@ -10,15 +10,25 @@
 class Window {
 public:
 	class Exception : public BaldoException {
+		using BaldoException::BaldoException;
 	public:
-		Exception(int line, const char* file, HRESULT hr) noexcept;
-		const char* what() const noexcept override;
-		virtual const char* GetType() const noexcept override;
 		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+	};
+	class HrException : public Exception {
+	public:
+		HrException(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
+
 		HRESULT GetErrorCode() const noexcept;
-		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
 	private:
 		HRESULT hr;
+	};
+	class NoGfxException : public Exception {
+	public:
+		using Exception::Exception;
+		const char* GetType() const noexcept override;
 	};
 	// singleton manages registration/cleanup of window class
 	class WindowClass {
@@ -57,5 +67,6 @@ private:
 };
 
 // error exception helper macro
-#define BHWND_EXCEPT( hr ) Window::Exception( __LINE__, __FILE__, hr )
-#define BHWND_LAST_EXCEPT() Window::Exception(__LINE__, __FILE__, GetLastError())
+#define BHWND_EXCEPT( hr ) Window::HrException( __LINE__, __FILE__, hr )
+#define BHWND_LAST_EXCEPT() Window::HrException(__LINE__, __FILE__, GetLastError())
+#define BHWND_NOGFX_EXCEPT() Window::NoGfxException(__LINE__, __FILE__)
